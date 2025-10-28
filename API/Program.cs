41b6f20bbf3fb985ecb68004f280ff5f245e1d45
BruhhnoV <brunoviottosantos@gmail.com>
2025-10-28 20:43:45 -0300

    feat: Update UI to fetch API songs

--- API/Program.cs
+++ API/Program.cs
@@ -1,17 +1,46 @@
+using System.Text.Json;
 using BeatAPI;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.ConfigureHttpJsonOptions(options =>
+{
+    options.SerializerOptions.PropertyNamingPolicy = JsonNamingPolicy.CamelCase;
+});
+
 // Configurar o SQLite
 builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseSqlite("Data Source=playlist.db"));
+{
+    options.UseSqlite("Data Source=playlist.db");
+});
 
-var app = builder.Build();
+builder.Services.AddCors(options => options
+    .AddPolicy(
+        name: "UI",
+        policy => policy
+            .AllowAnyHeader()
+            .AllowAnyMethod()
+            .WithOrigins("http://127.0.0.1:5500")
+    ));
+
+await using var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var context = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+
+    await context.Database.MigrateAsync();
+}
+
+app.UseCors("UI");
 
 // GET - Consultando todos os dados do banco de dados
-app.MapGet("/", async (AppDbContext db) =>
-    await db.Musicas.ToListAsync()
-);
+app.MapGet("/songs", async (AppDbContext db) =>
+{
+    var songs = await db.Musicas.ToArrayAsync();
 
-app.Run();
+    return Results.Ok(songs);
+});
+
+await app.RunAsync();
